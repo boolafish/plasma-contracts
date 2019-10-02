cee1be7730601a8445a23927e31ae668e3c08222
boolafish <boolafish945@gmail.com>
2019-10-02 14:14:37 +0900

    feat: take care of BondSize boundary number
    feat: block ability of setting lowerBoundDivisor to 0
    
    feat: BondSize fail the call when trying setting to 0
    
    Bond size would be stuck at 0 and become non-upgradeable if it ever goes to 0.
    Also, it makes no sense to have bond size 0 logically.
    
    test: BondSize boundary limits
    
    add tests with uint128 limit and 0 limit

--- plasma_framework/test/src/utils/BondSize.test.js
+++ plasma_framework/test/src/utils/BondSize.test.js
@@ -6,87 +6,195 @@ contract('BondSize', () => {
     const WAITING_PERIOD = time.duration.days(2);
     const HALF_WAITING_PERIOD = WAITING_PERIOD.divn(2);
 
-    beforeEach(async () => {
-        this.initialBondSize = new BN(20000000000);
-        this.lowerBoundDivisor = 2;
-        this.upperBoundMultiplier = 3;
-        this.contract = await BondSizeMock.new(
-            this.initialBondSize,
-            this.lowerBoundDivisor,
-            this.upperBoundMultiplier,
-        );
+    describe('buildParams', () => {
+        it('should fail when setting initialBondSize to 0', async () => {
+            const initialBondSize = new BN(0);
+            const lowerBoundDivisor = 2;
+            const upperBoundMultiplier = 3;
+
+            // BondSizeMock constructor would call buildParams under the hood
+            await expectRevert(
+                BondSizeMock.new(
+                    initialBondSize,
+                    lowerBoundDivisor,
+                    upperBoundMultiplier,
+                ),
+                'initialBondSize cannot be 0',
+            );
+        });
+
+        it('should fail when setting lowerBoundDivisor to 0', async () => {
+            const initialBondSize = new BN(20000000000);
+            const lowerBoundDivisor = 0;
+            const upperBoundMultiplier = 3;
+
+            // BondSizeMock constructor would call buildParams under the hood
+            await expectRevert(
+                BondSizeMock.new(
+                    initialBondSize,
+                    lowerBoundDivisor,
+                    upperBoundMultiplier,
+                ),
+                'lowerBoundDivisor cannot be 0',
+            );
+        });
+
+        it('should fail when setting upperBoundMultiplier to 0', async () => {
+            const initialBondSize = new BN(20000000000);
+            const lowerBoundDivisor = 2;
+            const upperBoundMultiplier = 0;
+
+            // BondSizeMock constructor would call buildParams under the hood
+            await expectRevert(
+                BondSizeMock.new(
+                    initialBondSize,
+                    lowerBoundDivisor,
+                    upperBoundMultiplier,
+                ),
+                'upperBoundMultiplier cannot be 0',
+            );
+        });
     });
 
-    it('should return the initial bond size', async () => {
-        const bondSize = await this.contract.bondSize();
-        expect(bondSize).to.be.bignumber.equal(this.initialBondSize);
-    });
+    describe('updateBondSize', () => {
+        describe('in general...', () => {
+            beforeEach(async () => {
+                this.initialBondSize = new BN(20000000000);
+                this.lowerBoundDivisor = 2;
+                this.upperBoundMultiplier = 3;
+                this.contract = await BondSizeMock.new(
+                    this.initialBondSize,
+                    this.lowerBoundDivisor,
+                    this.upperBoundMultiplier,
+                );
+            });
 
-    it('should be able to update the bond to upperBoundMultiplier times its current value', async () => {
-        const newBondSize = new BN(this.initialBondSize).muln(this.upperBoundMultiplier);
-        await this.contract.updateBondSize(newBondSize);
-    });
+            it('should return the initial bond size', async () => {
+                const bondSize = await this.contract.bondSize();
+                expect(bondSize).to.be.bignumber.equal(this.initialBondSize);
+            });
 
-    it('should fail to update bond to more than upperBoundMultiplier times its current value', async () => {
-        const newBondSize = new BN(this.initialBondSize).muln(this.upperBoundMultiplier).addn(1);
-        await expectRevert(
-            this.contract.updateBondSize(newBondSize),
-            'Bond size is too high',
-        );
-    });
+            it('should be able to update the bond to upperBoundMultiplier times its current value', async () => {
+                const newBondSize = new BN(this.initialBondSize).muln(this.upperBoundMultiplier);
+                await this.contract.updateBondSize(newBondSize);
+            });
 
-    it('should be able to update the bond to lowerBoundDivisor of its current value', async () => {
-        const newBondSize = new BN(this.initialBondSize).divn(this.lowerBoundDivisor);
-        await this.contract.updateBondSize(newBondSize);
-    });
+            it('should fail to update bond to more than upperBoundMultiplier times its current value', async () => {
+                const newBondSize = new BN(this.initialBondSize).muln(this.upperBoundMultiplier).addn(1);
+                await expectRevert(
+                    this.contract.updateBondSize(newBondSize),
+                    'Bond size is too high',
+                );
+            });
 
-    it('should fail to update bond to less than lowerBoundDivisor of its current value', async () => {
-        const newBondSize = new BN(this.initialBondSize).divn(this.lowerBoundDivisor).subn(1);
-        await expectRevert(
-            this.contract.updateBondSize(newBondSize),
-            'Bond size is too low',
-        );
-    });
+            it('should be able to update the bond to lowerBoundDivisor of its current value', async () => {
+                const newBondSize = new BN(this.initialBondSize).divn(this.lowerBoundDivisor);
+                await this.contract.updateBondSize(newBondSize);
+            });
 
-    it('should not update the actual bond value until after the waiting period', async () => {
-        const newBondSize = new BN(this.initialBondSize).muln(2);
-        await this.contract.updateBondSize(newBondSize);
+            it('should fail to update bond to less than lowerBoundDivisor of its current value', async () => {
+                const newBondSize = new BN(this.initialBondSize).divn(this.lowerBoundDivisor).subn(1);
+                await expectRevert(
+                    this.contract.updateBondSize(newBondSize),
+                    'Bond size is too low',
+                );
+            });
 
-        await time.increase(WAITING_PERIOD.sub(time.duration.seconds(5)));
-        const bondSize = await this.contract.bondSize();
-        expect(bondSize).to.be.bignumber.equal(this.initialBondSize);
-    });
+            it('should not update the actual bond value until after the waiting period', async () => {
+                const newBondSize = new BN(this.initialBondSize).muln(2);
+                await this.contract.updateBondSize(newBondSize);
 
-    it('should update the actual bond value after the waiting period', async () => {
-        const newBondSize = new BN(this.initialBondSize).muln(2);
-        await this.contract.updateBondSize(newBondSize);
+                await time.increase(WAITING_PERIOD.sub(time.duration.seconds(5)));
+                const bondSize = await this.contract.bondSize();
+                expect(bondSize).to.be.bignumber.equal(this.initialBondSize);
+            });
 
-        await time.increase(WAITING_PERIOD);
-        const bondSize = await this.contract.bondSize();
-        expect(bondSize).to.be.bignumber.equal(newBondSize);
-    });
+            it('should update the actual bond value after the waiting period', async () => {
+                const newBondSize = new BN(this.initialBondSize).muln(2);
+                await this.contract.updateBondSize(newBondSize);
 
-    it('should update the actual bond value after the waiting period', async () => {
-        const newBondSize = new BN(this.initialBondSize).muln(2);
-        await this.contract.updateBondSize(newBondSize);
+                await time.increase(WAITING_PERIOD);
+                const bondSize = await this.contract.bondSize();
+                expect(bondSize).to.be.bignumber.equal(newBondSize);
+            });
 
-        // Wait half the waiting period
-        await time.increase(HALF_WAITING_PERIOD);
+            it('should update the actual bond value after the waiting period', async () => {
+                const newBondSize = new BN(this.initialBondSize).muln(2);
+                await this.contract.updateBondSize(newBondSize);
 
-        // Update again while the first update is in progress.
-        const secondNewBondSize = new BN(this.initialBondSize).muln(1.5);
-        await this.contract.updateBondSize(secondNewBondSize);
+                // Wait half the waiting period
+                await time.increase(HALF_WAITING_PERIOD);
 
-        // Wait half the waiting period again.
-        await time.increase(HALF_WAITING_PERIOD);
-        // Even though the first update's waiting period is over, the second
-        // update is in progress so the bond size should not have changed yet.
-        let bondSize = await this.contract.bondSize();
-        expect(bondSize).to.be.bignumber.equal(this.initialBondSize);
+                // Update again while the first update is in progress.
+                const secondNewBondSize = new BN(this.initialBondSize).muln(1.5);
+                await this.contract.updateBondSize(secondNewBondSize);
 
-        // Wait the remaining waiting period
-        await time.increase(HALF_WAITING_PERIOD);
-        bondSize = await this.contract.bondSize();
-        expect(bondSize).to.be.bignumber.equal(secondNewBondSize);
+                // Wait half the waiting period again.
+                await time.increase(HALF_WAITING_PERIOD);
+                // Even though the first update's waiting period is over, the second
+                // update is in progress so the bond size should not have changed yet.
+                let bondSize = await this.contract.bondSize();
+                expect(bondSize).to.be.bignumber.equal(this.initialBondSize);
+
+                // Wait the remaining waiting period
+                await time.increase(HALF_WAITING_PERIOD);
+                bondSize = await this.contract.bondSize();
+                expect(bondSize).to.be.bignumber.equal(secondNewBondSize);
+            });
+        });
+
+        describe('with boundary size of numbers', () => {
+            it('should able to update to max number of uint128 without having overflow issue', async () => {
+                const maxSizeOfUint256 = (new BN(2)).pow(new BN(128)).sub(new BN(1)); // 2^128 - 1
+
+                const initialBondSize = maxSizeOfUint256.sub(new BN(10000));
+                const lowerBoundDivisor = 2;
+                const upperBoundMultiplier = 3;
+                const contract = await BondSizeMock.new(
+                    initialBondSize,
+                    lowerBoundDivisor,
+                    upperBoundMultiplier,
+                );
+
+                await contract.updateBondSize(maxSizeOfUint256);
+                await time.increase(WAITING_PERIOD + 1);
+                const bondSize = await contract.bondSize();
+                expect(bondSize).to.be.bignumber.equal(maxSizeOfUint256);
+            });
+
+            it('should able to update to 1', async () => {
+                const initialBondSize = new BN(2);
+                const lowerBoundDivisor = 2;
+                const upperBoundMultiplier = 3;
+                const contract = await BondSizeMock.new(
+                    initialBondSize,
+                    lowerBoundDivisor,
+                    upperBoundMultiplier,
+                );
+                const bondSizeOne = new BN(1);
+
+                await contract.updateBondSize(bondSizeOne);
+                await time.increase(WAITING_PERIOD + 1);
+                const bondSize = await contract.bondSize();
+                expect(bondSize).to.be.bignumber.equal(bondSizeOne);
+            });
+
+            it('should NOT be able to update to 0', async () => {
+                const initialBondSize = new BN(2);
+                const lowerBoundDivisor = 2;
+                const upperBoundMultiplier = 3;
+                const contract = await BondSizeMock.new(
+                    initialBondSize,
+                    lowerBoundDivisor,
+                    upperBoundMultiplier,
+                );
+                const bondSizeZero = new BN(0);
+
+                await expectRevert(
+                    contract.updateBondSize(bondSizeZero),
+                    'Bond size cannot be zero',
+                );
+            });
+        });
     });
 });
